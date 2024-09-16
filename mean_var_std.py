5166f59bb0df5f21a0b32a0735c3b083cbb9a2b1
Isabelle Aristide <ira9133@nyu.edu>
2024-09-16 22:30:15 +0000

    Add calculate function for mean, variance, std, etc.

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,7 +1,16 @@
 import numpy as np
 
 def calculate(list):
-
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    ar = np.array(list).reshape((3, 3))
+    calculations = {}
+    calculations['mean'] = [ar.mean(0).tolist(),ar.mean(1).tolist(),ar.mean()]
+    calculations['variance'] = [np.var(ar,0).tolist(),np.var(ar,1).tolist(),np.var(ar)]
+    calculations['standard deviation'] = [np.std(ar,0).tolist(),np.std(ar,1).tolist(),np.std(ar)]
+    calculations['max'] = [ar.max(0).tolist(),ar.max(1).tolist(),ar.max()]
+    calculations['min'] = [ar.min(0).tolist(), ar.min(1).tolist(),ar.min()]
+    calculations['sum'] = [ar.sum(0).tolist(),ar.sum(1).tolist(),ar.sum()]
 
 
 
